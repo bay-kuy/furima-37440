c59d5ac34c24600e381bfcd641364fc303325488
bay-kuy <keeper.4mr@gmail.com>
2022-03-02 14:11:54 +0900

    createアクションへのルーティングとコントローラー編集
    新規出品成否の分岐とストロングパラメーター設定。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,18 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_id, :prefecture_id,:ship_date_id, :price)
+  end
 end
