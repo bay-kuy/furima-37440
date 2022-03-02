35f4b787846b183041cae6a4386c1877f2e8f909
bay-kuy <keeper.4mr@gmail.com>
2022-03-02 21:23:50 +0900

    発送日付モデル名を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_id, :prefecture_id,:ship_date_id, :price)
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :ship_date_id, :price).merge(user_id: current_user.id)
   end
 end
