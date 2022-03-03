3e8f6d51ad716bd85b421e12fec16182fb5cb9cc
bay-kuy <keeper.4mr@gmail.com>
2022-03-03 14:31:29 +0900

    itemモデル単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Name.initials(number: 3)}
+    description {Faker::Lorem.sentence}
+    category { Category.all.sample }
+    condition { Condition.all.sample }
+    postage { Postage.all.sample }
+    prefecture { Prefecture.all.sample }
+    ship_date { ShipDate.all.sample }
+    price {Faker::Number.between(from: 300, to: 9999999)}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
