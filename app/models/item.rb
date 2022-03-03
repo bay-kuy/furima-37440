16b98d81c959d7122271e4442a9ac55aa252159f
bay-kuy <keeper.4mr@gmail.com>
2022-03-03 14:22:10 +0900

    itemモデル内の誤植修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   validates :image, :name, :description, presence: true
   validates :category_id, :condition_id, :postage_id, :prefecture_id,:ship_date_id, numericality: { message: "can't be blank"}
   with_options  presence: true, format: { with: /\A[0-9]+\z/ } do
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_ore_equal_to: 9_999_999 },
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
                       presence: { message: "can't be blank" }
   end
 end
