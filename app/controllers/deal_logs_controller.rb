c34d0067a1f57fae48b4ed2290f02536b639e275
bay-kuy <keeper.4mr@gmail.com>
2022-03-07 19:46:31 +0900

    商品情報表示のための変数定義

--- app/controllers/deal_logs_controller.rb
+++ app/controllers/deal_logs_controller.rb
@@ -1,6 +1,7 @@
 class DealLogsController < ApplicationController
   before_action :authenticate_user!, except: :index
   def index
+    @item = Item.find(params[:item_id])
     @deal_log_address = DealLogAddress.new
   end
 
