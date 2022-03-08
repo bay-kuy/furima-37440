a4b256354f5a1a30d856800aaa0287c023ba9bf9
bay-kuy <keeper.4mr@gmail.com>
2022-03-08 14:30:24 +0900

    商品購入ページへのアクセス制限
    非ログインユーザー、商品出品者が購入画面に遷移できないようにbefore actionを設定

--- app/controllers/deal_logs_controller.rb
+++ app/controllers/deal_logs_controller.rb
@@ -1,12 +1,14 @@
 class DealLogsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :set_item
+  before_action :move_to_items_index
+
+
   def index
-    @item = Item.find(params[:item_id])
     @deal_log_address = DealLogAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
+
     @deal_log_address = DealLogAddress.new(deal_log_params)
     if @deal_log_address.valid?
       @deal_log_address.save
@@ -20,5 +22,19 @@ class DealLogsController < ApplicationController
 
   def deal_log_params
     params.require(:deal_log_address).permit(:postcode, :prefecture_id, :city, :numbers, :building, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_items_index
+    if user_signed_in?
+      if current_user.id == @item.user_id
+        redirect_to root_path
+      end
+    else
+      redirect_to root_path
+    end
   end
 end
