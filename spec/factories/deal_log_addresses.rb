8fa718f17555539599875933315549eea363e896
bay-kuy <keeper.4mr@gmail.com>
2022-03-08 20:05:57 +0900

    商品購入機能モデルテスト実装

--- spec/factories/deal_log_addresses.rb
+++ spec/factories/deal_log_addresses.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :deal_log_address do
-    postcode         {123-4567}
+    postcode         {'123-4567'}
     prefecture_id    {14}
-    city             {横浜市緑区}
-    numbers          {青山1-1-1}
-    building         {柳ビル103}
+    city             {'横浜市緑区'}
+    numbers          {'青山1-1-1'}
+    building         {'柳ビル103'}
     telephone_number {Faker::Number.number(digits: 11)}
   end
 end
