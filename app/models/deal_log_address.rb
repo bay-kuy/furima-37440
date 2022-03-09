4c9da2cd598cc4ddc4dc11e624e8b7842ab5140e
bay-kuy <keeper.4mr@gmail.com>
2022-03-09 17:57:14 +0900

    バリデーション追加：トークン必須

--- app/models/deal_log_address.rb
+++ app/models/deal_log_address.rb
@@ -4,6 +4,7 @@ class DealLogAddress
 
   with_options presence: true do
     validates :user_id, :item_id, :city, :numbers
+    validates :token, presence: true
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :telephone_number, format: { with: /\A\d{10,11}\z/, message: "10桁もしくは11桁の半角数字を使用してください" }
