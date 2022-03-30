c51d89db9dd7d5d46cf046697cdaef9ba9cf39a3
bay-kuy <keeper.4mr@gmail.com>
2022-03-30 12:17:59 +0900

    preview.jsの読み込み

---
+++ app/javascript/preview.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const postForm = document.getElementById('item-image');
+  if (!postForm) return null;
+  console.log("preview.jsが読み込まれました");
+});
