0939588c6a62db98c810a540e42af9ca9c585eda
bay-kuy <keeper.4mr@gmail.com>
2022-03-31 13:40:52 +0900

    プレビュー画像を表示

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,14 +2,18 @@ document.addEventListener('DOMContentLoaded', function(){
   const postForm = document.getElementById('item-image');
   const previewList = document.getElementById('previews');
   if (!postForm) return null;
-  console.log("preview.jsが読み込まれました");
 
   const fileField = document.querySelector('input[type="file"][name="item[image]"]');
   fileField.addEventListener('change', function(e){
-    console.log("input要素で値の変化が起きました");
-    console.log(e.target.files[0]);
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
-    console.log(blob);
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
   });
 });
